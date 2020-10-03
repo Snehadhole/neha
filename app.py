6339ce1e5fbdf7ea815a9a7b158ae8e438f9c533
Snehadhole <50446219+Snehadhole@users.noreply.github.com>
2020-10-03 01:01:35 -0700

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def predict():
             
     int_features = [int(x) for x in request.form.values()]
     final_features = [np.array(int_features)]
-    prediction = model.predict(final_features)[0]
+    prediction = model.predict(final_features)
 
     output = round(prediction[0])
 
